0e6456628cea75734a6707c06c272e3684c8fbf5
Thomas Bell <t.bell@outlook.com>
2024-02-03 17:06:46 +0900

    Add food item and drink item arrays. Implement picker systems.

--- lunch_generator.js
+++ lunch_generator.js
@@ -1,12 +1,22 @@
 /* Lunch Generator Code Project
 Generates a random message, suggesting what you should eat for lunch. Includes a main, and a drink. */
+
+const foodItem = ['pizza', 'pasta', 'ramen', 'sushi', 'steak', 'a sandwhich', 'fried chicken', 'a burger', 'curry and rice']
+
+let foodPicker = Math.floor(Math.random() * foodItem.length)
+
+const drinkItem = ['milk', 'coffee', 'lemonade', 'boba', 'tea', 'cola', 'juice']
+
+let drinkPicker = Math.floor(Math.random() * drinkItem.length)
+
+
 const lunchStatement = () => {
   let statementPicker = Math.floor(Math.random() * 2);
 
   if (statementPicker === 0) {
-    return `Today, I suggest you should eat ${foodItem()} for lunch, accompanied by ${drinkItem()}!`
+    return `Today, I suggest you should eat ${foodItem[foodPicker]} for lunch, accompanied by a delicious glass of ${drinkItem[drinkPicker]}!`
   } else {
-    return `The Fates prophesy that you shall devour ${foodItem()} for lunch, along with ${drinkItem()}`
+    return `The Fates predict that you shall devour ${foodItem[foodPicker]} for lunch, along with a nice refreshing glass of ${drinkItem[drinkPicker]}.`
   }
 }
 
